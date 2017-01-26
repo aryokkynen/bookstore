f4a59fcb1637a7ca5eb813d38c73d4bb75c5dbfe
Antti Ryökkynen <antti.ryokkynen@myy.haaga-helia.fi>
2017-01-26 19:36:28 +0200

    First commit

---
+++ src/main/java/com/antti/bookstore/domain/Book.java
@@ -0,0 +1,72 @@
+package com.antti.bookstore.domain;
+
+public class Book {
+
+	private String title;
+	private String author;
+	private String isbn;
+
+	private double price;
+	private int year;
+
+	public Book(String title, String author, String isbn, double price, int year) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.price = price;
+		this.year = year;
+	}
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn
+				+ ", price=" + price + ", year=" + year + "]";
+	}
+
+}
---
+++ src/main/java/com/antti/bookstore/web/BookController.java
@@ -0,0 +1,19 @@
+package com.antti.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@ResponseBody
+public class BookController {
+	
+	
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "Bookstore";
+	}
+
+}
